d9150b0307fa96e6980770044aca361893936e6b
Andrei Popov <andrepopoff@gmail.com>
2021-11-12 22:36:36 +0300

    Create gltf model

--- js/main.js
+++ js/main.js
@@ -4,3 +4,35 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const light = new THREE.AmbientLight( 0xffffff, 0.5 );
+scene.add( light );
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+camera.position.z = 5;
+
+const loader = new THREE.GLTFLoader();
+loader.load('../models/scene.gltf', function( gltf ) {
+    scene.add( gltf.scene );
+    gltf.scene.scale.set(15, 15, 15);
+  }
+);
+
+function animate() {
+  renderer.render( scene, camera );
+  requestAnimationFrame( animate );
+}
+animate();
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
